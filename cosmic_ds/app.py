22638bf3b3b605fc9ac28609ca0dc9d0dc7e54c4
Nicholas Earl <contact@nicholasearl.me>
2021-06-17 11:57:19 -0500

    Setup vue template syncing with app class

--- cosmic_ds/app.py
+++ cosmic_ds/app.py
@@ -1,8 +1,17 @@
 from echo import CallbackProperty, DictCallbackProperty, ListCallbackProperty
 from glue.core.state_objects import State
 from glue_jupyter.app import JupyterApplication
+from glue_jupyter.bqplot import scatter
 from glue_jupyter.state_traitlets_helpers import GlueState
 from ipyvuetify import VuetifyTemplate
+from .utils import load_template
+from traitlets import Dict, Bool, List, Int
+from glue_jupyter.bqplot.profile import BqplotProfileView
+from glue_jupyter.bqplot.image import BqplotImageView
+from ipywidgets import widget_serialization
+import numpy as np
+from glue_jupyter.vuetify_layout import vuetify_layout_factory
+
 
 
 class ApplicationState(State):
@@ -17,10 +26,31 @@ class ApplicationState(State):
     e1 = CallbackProperty(1)
 
 
-class Application(VuetifyTemplate, JupyterApplication):
+class Application(VuetifyTemplate):
     _metadata = Dict({"mount_id": "content"}).tag(sync=True)
     state = GlueState().tag(sync=True)
     template = load_template("app.vue", __file__).tag(sync=True)
+    viewers = Dict().tag(sync=True, **widget_serialization)
+    items = List().tag(sync=True)
     
-    def __init__(self, configuration=None, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.state = ApplicationState()
+        self._application_handler = JupyterApplication()
+        self._application_handler.load_data("/Users/nmearl/Downloads/w5/w5.fits")
+
+        image_viewer = self._application_handler.imshow(show=False)
+        image_viewer_layout = vuetify_layout_factory(image_viewer)
+
+        scatter_viewer = self._application_handler.scatter2d(x='Right Ascension', y='Declination', show=False)
+        scatter_viewer_layout = vuetify_layout_factory(scatter_viewer)
+
+        self.viewers = {
+            'image_viewer': image_viewer_layout, 
+            'profile_viewer': scatter_viewer_layout
+        }
+
+    @property
+    def session(self):
+        return self._application_handler.session
---
+++ cosmic_ds/app.vue
@@ -0,0 +1,180 @@
+<template>
+  <v-app id="cosmic-ds-app">
+    <v-app-bar
+      color="primary"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://cdn.eso.org/images/screen/eso1738b.jpg"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-4"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Hubble Data Story</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-menu bottom left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon color="yellow" v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Collect Galaxy Data</v-tab>
+          <v-tab>Analysis Tools</v-tab>
+          <v-tab>View Results</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <!-- <v-sheet id="scrolling-techniques-4" class="overflow-y-auto fill-height" style="height:100%"> -->
+    <v-main id="scrolling-techniques-4" class="fill-height">
+      <v-container class="fill-height">
+        <v-card
+          class="fill-height overflow-y-auto d-flex flex-column"
+          style="width: 100%"
+        >
+          <v-stepper v-model="state.e1" class="fill-height elevation-0">
+            <v-stepper-header>
+              <v-stepper-step :complete="state.e1 > 1" step="1" editable>
+                Collect Galaxy Data
+              </v-stepper-step>
+
+              <v-divider></v-divider>
+
+              <v-stepper-step :complete="state.e1 > 2" step="2" editable>
+                Analysis Tools
+              </v-stepper-step>
+
+              <v-divider></v-divider>
+
+              <v-stepper-step :complete="state.e1 > 2" step="3" editable
+                >View Results</v-stepper-step
+              >
+            </v-stepper-header>
+
+            <v-stepper-items class="fill-height">
+              <v-stepper-content step="1">
+                <v-card
+                  class="fill-height mb-12"
+                  color="grey lighten-1 elevation-0"
+                ></v-card>
+
+                <v-btn color="primary" @click="state.e1 = 2"> Continue </v-btn>
+
+                <v-btn text> Cancel </v-btn>
+              </v-stepper-content>
+
+              <v-stepper-content step="2">
+                <v-card
+                  class="fill-height mb-12"
+                  color="grey lighten-1 elevation-0"
+                ></v-card>
+
+                <v-btn color="primary" @click="state.e1 = 3"> Continue </v-btn>
+
+                <v-btn text> Cancel </v-btn>
+              </v-stepper-content>
+
+              <v-stepper-content step="3">
+                <v-card
+                  class="fill-height mb-12"
+                  color="grey lighten-1 elevation-0"
+                ></v-card>
+
+                <v-btn color="primary" @click="state.e1 = 1"> Continue </v-btn>
+
+                <v-btn text> Cancel </v-btn>
+              </v-stepper-content>
+            </v-stepper-items>
+          </v-stepper>
+          <v-card-actions>
+            <v-btn color="primary" @click="state.e1 = 1"> Continue </v-btn>
+
+            <v-btn text> Cancel </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </v-main>
+
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in state.footer_link_items"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>CosmicDS Project</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
+</template>
+
+<style id="cosmic-ds-app">
+html {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.jupyter-widgets .jp-Cell .jp-CodeCell .jp-Notebook-cell .jp-mod-noInput {
+  margin: 0;
+  padding: 0;
+}
+
+#cosmic-ds-app {
+  height: 100%;
+}
+
+#app {
+  height: 100vh;
+}
+
+.card-outter {
+  position: relative;
+  padding-bottom: 50px;
+}
+.card-actions {
+  position: absolute;
+  bottom: 0;
+}
+.v-stepper__wrapper {
+  height: 100%;
+}
+</style>
