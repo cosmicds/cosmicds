7a817cfff4230a82c2bba52021a8ee35fca1ca73
Nicholas Earl <contact@nicholasearl.me>
2021-08-07 15:51:54 -0500

    Populate custom pure Vue component registry

--- cosmicds/__init__.py
+++ cosmicds/__init__.py
@@ -2,3 +2,15 @@
 
 from .version import __version__
 __all__ = []
+
+# Register any custom Vue components
+from pathlib import Path
+import ipyvue
+
+vue_comp_dir = Path(__file__).parent / "data" / "vue_components"
+
+for comp_path in vue_comp_dir.iterdir():
+    if comp_path.suffix == ".vue":
+        ipyvue.register_component_from_string(
+            name=comp_path.stem.replace('_', '-'),
+            value=comp_path.read_text())
