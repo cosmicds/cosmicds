e0a3796add70b023a988a3daecb8fcf70cdfa5db
Jon Carifio <carifio24@gmail.com>
2021-10-04 11:55:47 -0400

    Modify tests to use testbook and Jupyter notebooks to handle the kernel issue caused by the new release of pywwt.

--- cosmicds/tests/test_basic.py
+++ cosmicds/tests/test_basic.py
@@ -1,10 +1,10 @@
-import pytest
+from testbook import testbook
+from pathlib import Path
+from os.path import join
 
+directory = str(Path(__file__).parent)
 
-def test_instantiate():
-    from cosmicds.app import Application
-    from glue.core.state_objects import State
-
-    app = Application()
-
-    assert issubclass(type(app.state), State)
+@testbook(join(directory, "test_basic.ipynb"), execute=True)
+def test_instantiate(tb):
+    instantiate = tb.ref("test_instantiate")
+    assert instantiate()
