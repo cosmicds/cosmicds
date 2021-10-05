42d533fa502ef8c12d0c463b88a7a381566b2aa8
Jon Carifio <carifio24@gmail.com>
2021-10-05 12:56:53 -0400

    Modify table component to be more general-purpose. Remove demo data.

--- cosmicds/components/table/table.py
+++ cosmicds/components/table/table.py
@@ -1,8 +1,9 @@
 from glue.core.hub import Hub
 import numpy as np
 from glue.core.message import (DataCollectionAddMessage,
-                               DataCollectionDeleteMessage)
+                               DataCollectionDeleteMessage, DataUpdateMessage)
 from glue.core import HubListener
+from glue.core.subset import SubsetState
 from ipyvuetify import VuetifyTemplate
 from traitlets import Bool, List, Unicode
 
@@ -12,38 +13,41 @@ __all__ = ['Table']
 
 
 class Table(VuetifyTemplate, HubListener):
+    default_color = '#00ff00'
+
     template = load_template("table.vue", __file__).tag(sync=True)
-    headers = List([
-        {
-            'text': 'Student ID',
-            'value': 'student_id',
-            'align': 'start'
-        },
-        {
-            'text': 'Distance',
-            'value': 'distance'
-        },
-        {
-            'text': 'Velocity',
-            'value': 'velocity'
-        },
-        {
-            'text': 'Type',
-            'value': 'type'
-        }]).tag(sync=True)
+    headers = List().tag(sync=True)
     items = List().tag(sync=True)
+    key_component = Unicode().tag(sync=True)
     search = Unicode().tag(sync=True)
     single_select = Bool(False).tag(sync=True)
     selected = List().tag(sync=True)
+    use_search = Bool(False).tag(sync=True)
 
-    def __init__(self, session, *args, **kwargs):
+    def __init__(self, session, data, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self._session = session
-        self._subset_group = None
+        self._subset_group = kwargs.get('subset_group', None)
+        if self._subset_group is not None:
+            self._subset_group_label = self._subset_group.label
+        else:
+            self._subset_group_label = "selected"
+
+        components = kwargs.get('glue_components', [x.label for x in data.components])
+        self.key_component = kwargs.get('key_component', components[0])
+        self.glue_data = data
+        self._data_label = data.label
+
+        self.glue_components = components
+        self.glue_component_names = kwargs.get('names', components)
+        self._message_filter = lambda message: message.attribute in self.glue_components
+
+        self.single_select = kwargs.get('single_select', False)
+        self.subset_color = kwargs.get('color', Table.default_color)
 
         # Populate the table with the current data in the collection
-        self._on_data_collection_updated()
+        self._populate_table()
 
         # Subscribe to change events that alter the data collection so that
         # the table can be updated accordingly
@@ -53,7 +57,7 @@ class Table(VuetifyTemplate, HubListener):
                            handler=self._on_data_collection_updated)
 
         # Listen for changes to selected rows
-        self.observe(self.on_selected_changed, names=['selected'])
+        self.observe(self._on_selected_changed, names=['selected'])
 
     @property
     def data_collection(self):
@@ -63,25 +67,56 @@ class Table(VuetifyTemplate, HubListener):
     def hub(self):
         return self._session.hub
 
-    def _on_data_collection_updated(self):
-        df = self.data_collection['random_data0'].to_dataframe()
+    def _subset_state_from_selected(self, selected):
+        keys = [x[self.key_component] for x in selected]
+        if keys:
+            state = np.bitwise_or.reduce([self.glue_data.id[self.key_component] == x for x in keys])
+        else:
+            state = SubsetState()
+        return state
+
+    def _selection_from_state(self, state):
+        mask = state.to_mask()
+        return [item for index, item in enumerate(self.items) if mask[index]]
+
+    def _populate_table(self):
+        df = self.glue_data.to_dataframe()
+        self.headers = [{
+            'text': self.glue_component_names[index],
+            'value': self.glue_components[index]
+        } for index in range(len(self.glue_components))]
+        self.headers[0]['align'] = 'start'
         self.items = [
             {
-                'student_id': row.student_id,
-                'distance': row.distance,
-                'velocity': row.velocity,
-                'type': row.galaxy_type
+                component: getattr(row, component) for component in self.glue_components
             } for row in df.itertuples()
         ]
 
-    def on_selected_changed(self, event):
-        student_ids = [x['student_id'] for x in event['new']]
-        data = self.data_collection['random_data0']
-        state = np.bitwise_or.reduce([data.id['student_id'] == x for x in student_ids])
+    def _on_data_added(self):
+        self.glue_data = self.data_collection[self._data_label]
+        state = self._subset_state_from_selected(self.selected)
+        self._subset_group = self.data_collection.new_subset_group(self._subset_group_label, state)
+        self._subset_group.style.color = self.subset_color
 
-        # Remove previous subset group
+    # def _on_data_updated(self):
+    #     self.selected = self._selection_from_state(self._subset_group.state)
+
+    def _on_data_deleted(self):
         self.data_collection.remove_subset_group(self._subset_group)
+        self._subset_group = None
 
-        # Add new subset group
-        self._subset_group = self.data_collection.new_subset_group("selected", state)
-        self._subset_group.style.color = '#00ff00'
+    def _on_data_collection_updated(self, message=None):
+        if message is not None and message.data.label == self._data_label:
+            if isinstance(message, DataCollectionAddMessage):
+                self._on_data_added()
+            else:
+                self._on_data_deleted()
+        self._populate_table()
+        
+    def _on_selected_changed(self, _event):
+        state = self._subset_state_from_selected(_event['new'])
+        if self._subset_group is None:
+            self._subset_group = self.data_collection.new_subset_group(self._subset_group_label, state)
+        else:
+            self._subset_group.state = state
+
