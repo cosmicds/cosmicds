995f038abb4063489474e836df983b290aa1c666
Jonathan Carifio <carifio24@gmail.com>
2022-01-07 12:43:22 +0000

    Refactor so that the string formatting for angles is done in the app, rather than by the measuring tool. The traits stored in the measuring tool are now astropy Quantities.

--- cosmicds/components/measuring_tool/measuring_tool.py
+++ cosmicds/components/measuring_tool/measuring_tool.py
@@ -4,22 +4,32 @@ from cosmicds.utils import load_template
 from traitlets import Instance, Bool, Float, Int, Unicode, observe
 from ipywidgets import DOMWidget, widget_serialization
 
+def quantity_to_json(quantity, _widget):
+    return {
+        "value": quantity.value,
+        "unit": quantity.unit.name
+    }
+
+def quantity_from_json(jsn, _widget):
+    return jsn["value"] * u.Unit(jsn["unit"])
+
 class MeasuringTool(v.VueTemplate):
     template = load_template("measuring_tool.vue", __file__).tag(sync=True)
     widget = Instance(DOMWidget, allow_none=True).tag(sync=True, **widget_serialization)
     measuring = Bool().tag(sync=True)
     measuredDistance = Float().tag(sync=True)
-    angular_distance = Float().tag(sync=True)
-    angular_distance_str = Unicode("0°").tag(sync=True)
-    angular_height_str = Unicode("60°").tag(sync=True)
+    angular_distance = Instance(u.Quantity).tag(sync=True, to_json=quantity_to_json, from_json=quantity_from_json)
+    angular_height = Instance(u.Quantity).tag(sync=True, to_json=quantity_to_json, from_json=quantity_from_json)
     height = Int().tag(sync=True)
     width = Int().tag(sync=True)
     
     def __init__(self, wwt, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         self.widget = wwt
         self.measuring = kwargs.get('measuring', False)
+        self.angular_distance = 0 * u.deg
+        self.angular_height = 60 * u.deg
         self.widget._set_message_type_callback('wwt_view_state', self._on_fov_change)
+        super().__init__(*args, **kwargs)
         
     def reset_canvas(self):
         self.send({"method": "reset", "args": []})
@@ -31,9 +41,7 @@ class MeasuringTool(v.VueTemplate):
     def _on_measured_distance_changed(self, change):
         fov = self.widget.get_fov()
         widget_height = self._height_from_pixel_str(self.widget.layout.height)
-        angular_dist = ((change["new"] / widget_height) * fov)
-        self.angular_distance = angular_dist.value # In degrees
-        self.angular_distance_str = self.format_angle(angular_dist)
+        self.angular_distance = ((change["new"] / widget_height) * fov)
         
     @observe('measuring')
     def _on_measuring_changed(self, measuring):
@@ -41,19 +49,6 @@ class MeasuringTool(v.VueTemplate):
             self.reset_canvas()
 
     def _on_fov_change(self, wwt, _updated):
-        fov = self.widget.get_fov().to(u.deg)
-        self.angular_height = fov
-        self.angular_height_str = self.format_angle(fov)
+        self.angular_height = self.widget.get_fov()
 
-    def format_angle(self, angle):
-        angle_deg = angle.to(u.deg)
-        if angle_deg.value >= 1:
-            return f"{angle_deg.value:.2f}°"
-
-        angle_min = angle_deg.to(u.arcmin)
-        if angle_min.value >= 1:
-            return f"{angle_min.value:.2f}′"
-        
-        angle_asec = angle_min.to(u.arcsec)
-        return f"{angle_asec.value:.2f}″"
 
