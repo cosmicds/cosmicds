243efeb679ddd011b3be3d6258076ea1893a920f
Jon Carifio <carifio24@gmail.com>
2022-01-14 11:31:53 -0500

    Modify the information card for the measuring tool to show the viewer FOV and measured distance, both of which update when the tool value does.

--- cosmicds/components/measuring_tool/measuring_tool.py
+++ cosmicds/components/measuring_tool/measuring_tool.py
@@ -1,7 +1,7 @@
 import os
 import ipyvue as v
 from cosmicds.utils import load_template
-from traitlets import Instance, Bool, Float, Int, observe
+from traitlets import Instance, Bool, Float, Int, Unicode, observe
 from ipywidgets import DOMWidget, widget_serialization
 
 class MeasuringTool(v.VueTemplate):
@@ -10,6 +10,8 @@ class MeasuringTool(v.VueTemplate):
     measuring = Bool().tag(sync=True)
     measuredDistance = Float().tag(sync=True)
     angular_distance = Float().tag(sync=True)
+    angular_distance_str = Unicode("0°").tag(sync=True)
+    angular_height_str = Unicode("60°").tag(sync=True)
     height = Int().tag(sync=True)
     width = Int().tag(sync=True)
 
@@ -17,8 +19,7 @@ class MeasuringTool(v.VueTemplate):
         super().__init__(*args, **kwargs)
         self.widget = wwt
         self.measuring = kwargs.get('measuring', False)
-        self.height = self._height_from_pixel_str(self.widget.layout.height or '400px')
-        self.width = self._height_from_pixel_str(self.widget.layout.width or '500px')
+        self.widget._set_message_type_callback('wwt_view_state', self._on_fov_change)
 
     def reset_canvas(self):
         self.send({"method": "reset", "args": []})
@@ -30,9 +31,28 @@ class MeasuringTool(v.VueTemplate):
     def _on_measured_distance_changed(self, change):
         fov = self.widget.get_fov()
         widget_height = self._height_from_pixel_str(self.widget.layout.height)
-        self.angular_distance = ((change["new"] / widget_height) * fov).value
+        angular_dist = ((change["new"] / widget_height) * fov)
+        self.angular_distance = angular_dist.value # In degrees
+        self.angular_distance_str = self.format_angle(angular_dist)
 
     @observe('measuring')
     def _on_measuring_changed(self, measuring):
         if not measuring["new"]:
             self.reset_canvas()
+
+    def _on_fov_change(self, wwt, _updated):
+        fov = self.widget.get_fov().to(u.deg)
+        self.angular_height = fov
+        self.angular_height_str = self.format_angle(fov)
+
+    def format_angle(self, angle):
+        angle_deg = angle.to(u.deg)
+        if angle_deg.value >= 1:
+            return f"{angle_deg.value:.2f}°"
+
+        angle_min = angle_deg.to(u.arcmin)
+        if angle_min.value >= 1:
+            return f"{angle_min.value:.2f}′"
+
+        angle_asec = angle_min.to(u.arcsec)
+        return f"{angle_asec.value:.2f}″"
