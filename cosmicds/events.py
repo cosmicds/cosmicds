42d9f903798198fdbbac985c67451458ae4f1803
Nicholas Earl <contact@nicholasearl.me>
2022-02-18 12:17:19 -0600

    Include new glue events for database writing

--- cosmicds/events.py
+++ cosmicds/events.py
@@ -1,15 +1,20 @@
 from glue.core.message import Message
 
 
-class StepChangeMessage(Message):
-    def __init__(self, value, *args, **kwargs):
+class WriteToDatabaseMessage(Message):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self._value = value
+
+class StepChangeMessage(Message):
+    def __init__(self, step, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._step = step
 
     @property
-    def value(self):
-        return self._value
+    def step(self):
+        return self._step
 
 
 class LoadDataMessage(Message):
@@ -26,3 +31,19 @@ class LoadDataMessage(Message):
     @property
     def label(self):
         return self._label
+
+
+class NewViewerMessage(Message):
+    def __init__(self, viewer_class, data=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._viewer_class = viewer_class
+        self._data = data
+
+    @property
+    def viewer_class(self):
+        return self._viewer_class
+
+    @property
+    def data(self):
+        return self._data
