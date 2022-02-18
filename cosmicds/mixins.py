9195255546d2e2965bdc6a58fe5a4701099f0eb4
Nicholas Earl <contact@nicholasearl.me>
2022-02-18 12:17:48 -0600

    Add mixin helpers for templates

--- cosmicds/mixins.py
+++ cosmicds/mixins.py
@@ -1,18 +1,26 @@
 from ipyvuetify import VuetifyTemplate
 from glue.core import HubListener
 
-__all__ = ['TemplateMixin']
+__all__ = ['HubMixin', 'TemplateMixin']
 
 
-class TemplateMixin(VuetifyTemplate, HubListener):
+class HubMixin(HubListener):
+    @property
+    def app(self):
+        return self._session.application
+
     @property
     def hub(self):
-        return self._app.session.hub
+        return self._session.hub
 
     @property
     def session(self):
-        return self._app.session
+        return self._session
 
     @property
     def data_collection(self):
-        return self._app.session.data_collection
+        return self._session.data_collection
+
+
+class TemplateMixin(VuetifyTemplate, HubMixin):
+    pass
