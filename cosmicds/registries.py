6f89177c87d744172bc729304a4ddc6fa37f82e8
Nicholas Earl <contact@nicholasearl.me>
2022-03-04 16:24:38 -0600

    Add private reference to stage instance in story

--- cosmicds/registries.py
+++ cosmicds/registries.py
@@ -67,7 +67,8 @@ class StoryRegistry(UniqueDictRegistry):
                                      "step_index": 0,
                                      "steps": [{'title': x, 'completed': False} 
                                                for x in v['steps']],
-                                     "model_id": f"IPY_MODEL_{stage.model_id}"}
+                                     "model_id": f"IPY_MODEL_{stage.model_id}",
+                                     "_instance": stage}
 
         return story_state
 
