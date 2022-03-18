9f4aa177eb7f296a986f1bd7a9ef6a6bb0f1a219
Jon Carifio <carifio24@gmail.com>
2022-03-18 15:23:24 -0400

    Import custom vue components in the Hubble story as well.

--- cosmicds/stories/hubbles_law/__init__.py
+++ cosmicds/stories/hubbles_law/__init__.py
@@ -1,2 +1,12 @@
 from .state import *
 from .stages import *
+
+import ipyvue
+
+vue_comp_dir = Path(__file__).parent / "vue_components"
+
+for comp_path in vue_comp_dir.iterdir():
+    if comp_path.suffix == ".vue":
+        ipyvue.register_component_from_string(
+            name=comp_path.stem.replace('_', '-'),
+            value=comp_path.read_text())
