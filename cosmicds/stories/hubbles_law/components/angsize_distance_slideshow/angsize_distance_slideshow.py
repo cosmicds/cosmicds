9666398c1636a1e06b271a7949e54a8bc4a5ea0e
LilyN11 <lilynguyen1011@gmail.com>
2022-03-28 15:07:01 -0400

    stage 2 slides

---
+++ cosmicds/stories/hubbles_law/components/angsize_distance_slideshow/angsize_distance_slideshow.py
@@ -0,0 +1,48 @@
+import ipyvuetify as v
+from pathlib import Path
+from traitlets import Int, Bool, Unicode
+from cosmicds.utils import load_template
+from glue_jupyter.state_traitlets_helpers import GlueState
+
+
+#theme_colors()
+
+class Angsize_SlideShow(v.VuetifyTemplate):
+    template = load_template("angsize_distance_slideshow.vue", __file__, traitlet=True).tag(sync=True)
+    step = Int(0).tag(sync=True)
+    length = Int(9).tag(sync=True)
+    dialog = Bool(False).tag(sync=True)
+    currentTitle = Unicode("").tag(sync=True)
+    state = GlueState().tag(sync=True)
+    #exploration_complete = Bool(False).tag(sync=True)
+    #intro_complete = Bool(False).tag(sync=True)
+
+    _titles = [
+        "Light and Spectra",
+        "Light and Spectra", 
+        "Light and Spectra",
+        "Light and Spectra",
+        "Doppler Shift",
+        "Doppler Shift",
+        "Atom & Molecule Emissions",
+        "Emission & Absorption",
+        "Complete"
+    ]
+    _default_title = "Light and Spectra"
+
+    def __init__(self, story_state, *args, **kwargs):
+        self.state = story_state
+        self.currentTitle = self._default_title
+
+        def update_title(change):
+            index = change["new"]
+            if index in range(len(self._titles)):
+                self.currentTitle = self._titles[index]
+            else:
+                self.currentTitle = self._default_title
+
+        self.observe(update_title, names=["step"])
+
+        super().__init__(*args, **kwargs)
+
+
--- cosmicds/stories/hubbles_law/stages/stage_two.vue
+++ cosmicds/stories/hubbles_law/stages/stage_two.vue
@@ -3,15 +3,19 @@
     <v-row class="d-flex align-stretch">
       <v-col cols="6">
         <v-card class="align-self-stretch">
-          <c-distance-tool/>
+          <c-distance-tool />
         </v-card>
       </v-col>
       <v-col cols="3">
-        <c-distance-sidebar/>
+        <c-distance-sidebar />
       </v-col>
     </v-row>
     <v-row>
-      <jupyter-widget :widget="widgets.distance_table"/>
+      <jupyter-widget :widget="widgets.distance_table" />
+      <v-col cols="3">
+        <!-- FORM DIALOG as template for reflections/MC -->
+        <c-angsize-slideshow />
+      </v-col>
     </v-row>
   </v-container>
 </template>
