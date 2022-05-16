7e913c8e6ed055190668085646aa35bbbc421335
Jon Carifio <carifio24@gmail.com>
2022-05-16 16:29:44 -0400

    Add scorekeeping functionality to the multiple choice component.

--- cosmicds/vue_components/mc_radiogroup.vue
+++ cosmicds/vue_components/mc_radiogroup.vue
@@ -29,18 +29,32 @@
         </span>
       </div>
     </div>
+    <div
+      v-if="complete"
+      class="text-right">
+      <span class="yellow--text">
+        {{ `Score: ${score(tries)} points` }}
+      </span>
+    </div>
+
   </v-container>
 </template>
 
 <script>
 module.exports = {
-  props: [
-    "radioOptions",
-    "correctAnswers",
-    "neutralAnswers",
-    "feedbacks",
-    "selectedCallback"
-  ],
+  props: {
+    correctAnswers: Array,
+    feedbacks: Array,
+    neutralAnswers: Array,
+    points: {
+      type: [Array, Function],
+      default(_rawProps) {
+        return function(ntries) { return Math.max(10 - 2 * ntries, 0); };
+      }
+    },
+    radioOptions: Array,
+    selectedCallback: Function
+  },
   data: function () {
     return {
       column: null,
@@ -49,20 +63,30 @@ module.exports = {
       colorWrong: 'red',
       complete: false,
       feedbackIndex: null,
+      tries: 0,
     };
   },
   methods: {
     selectChoice: function(index) {
       this.feedbackIndex = index;
+      this.tries += 1;
       if (this.selectedCallback != null) {
         this.selectedCallback(index);
       }
       if (this.correctAnswers.includes(index)) {
         this.complete = true;
       }
+      console.log(this.complete);
     },
     color: function(index) {
       return this.correctAnswers?.includes(index) ? this.colorRight : this.neutralAnswers?.includes(index) ? this.colorNeutral : this.colorWrong;
+    },
+    score: function(ntries) {
+      if (Array.isArray(this.points)) {
+        return this.points[ntries-1];
+      } else {
+        return this.points(ntries);
+      }
     }
   }
 };
