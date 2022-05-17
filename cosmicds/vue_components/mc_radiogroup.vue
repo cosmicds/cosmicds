31e3e8f8d564526f2bdcf3e9faa62d923efdda77
Pat Udomprasert <pudompra@cfa.harvard.edu>
2022-05-17 10:43:17 -0400

    Update to interpret feedback text as html instead of strings
    per @Carifio24's comment in this thread.

--- cosmicds/vue_components/mc_radiogroup.vue
+++ cosmicds/vue_components/mc_radiogroup.vue
@@ -25,8 +25,10 @@
       <div
         :class="feedbackIndex !== null ? 'd-block' : 'd-none'"
       >
-        <span :class="`${color(feedbackIndex)}--text`">
-          {{ feedbacks[feedbackIndex] }}
+        <span
+          :class="`${color(feedbackIndex)}--text`" 
+          v-html="feedbacks[feedbackIndex]"
+        >
         </span>
       </div>
     </div>
