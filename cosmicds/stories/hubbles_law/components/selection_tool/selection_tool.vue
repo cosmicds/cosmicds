21718b74924444f2347daacbae9eda70cfd54e91
Pat Udomprasert <pudompra@cfa.harvard.edu>
2022-06-07 17:46:27 -0400

    Typo fix

--- cosmicds/stories/hubbles_law/components/selection_tool/selection_tool.vue
+++ cosmicds/stories/hubbles_law/components/selection_tool/selection_tool.vue
@@ -91,7 +91,7 @@
               >
                 <v-col>
                   <p>
-                    The Comsic Sky Viewer shows a modern data set from the Sloan Digital Sky Survey (SDSS), which has collected imaging and spectral data for millions of galaxies. The green dots mark the locations of galaxies you can collect data for.
+                    The Cosmic Sky Viewer shows a modern data set from the Sloan Digital Sky Survey (SDSS), which has collected imaging and spectral data for millions of galaxies. The green dots mark the locations of galaxies you can collect data for.
                   </p>
                   <v-row>
                     <v-col
