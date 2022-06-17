a096e055ab73b36828307575b2754775dde8620a
Pat Udomprasert <pudompra@cfa.harvard.edu>
2022-06-16 22:52:03 -0400

    Remove Nick name and email; replace with guest student + ID number generated by db

--- cosmicds/app.vue
+++ cosmicds/app.vue
@@ -73,12 +73,8 @@
 
             <v-list-item-content>
               <v-list-item-title>
-                Nicholas Earl
+                Guest Student {{ student_id }}
               </v-list-item-title>
-              <v-list-item-subtitle>
-                nearl@gluesolutions.io
-              </v-list-item-subtitle
-              >
             </v-list-item-content>
           </v-list-item>
         </v-list>
