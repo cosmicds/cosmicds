dd82fc1a76d86901fde394ec2a704183e27473e2
Harry Houghton <henry.houghton@cfa.harvard.edu>
2022-06-23 00:26:06 -0400

    Revert "update naming conventions for merge with main"
    This reverts commit abc1280be2ee3358e79179fbc309086e868a4d38.

---
+++ cosmicds/stories/hubbles_law/components/generic_state_components/stage_one/restwave_alert.vue
@@ -0,0 +1,25 @@
+<template>
+  <scaffold-alert
+    header-text="Rest Wavelength"
+    @back="
+      state.marker = 'mee_spe1';
+    "
+    @next="
+      state.marker = 'obs_wav1';
+    "
+  >
+    <div
+      class="mb-4"
+    >
+      <p>
+        Your galaxy spectrum has either an H-&alpha; emission line or a Mg-I absorption line marked.
+      </p>
+      <p>
+        The known <strong>rest wavelength</strong> for the line is recorded in your table.
+      </p>
+      <p>
+        Click the <v-icon>mdi-lambda</v-icon> icon in the viewer header to display the rest wavelength for that line. 
+      </p>
+    </div>
+  </scaffold-alert>
+</template>
