928360b287fc544cf067a630c8b3cad7f08b7c58
Jon Carifio <carifio24@gmail.com>
2022-06-28 16:05:30 -0400

    Update top-level __init__ to account for folder reorganization.

--- cosmicds/__init__.py
+++ cosmicds/__init__.py
@@ -7,10 +7,10 @@ __all__ = []
 from pathlib import Path
 import ipyvue
 
-vue_comp_dir = Path(__file__).parent / "vue_components"
+comp_dir = Path(__file__).parent / "components"
 
-for comp_path in vue_comp_dir.iterdir():
-    if comp_path.suffix == ".vue":
+for comp_path in comp_dir.iterdir():
+    if comp_path.is_file and comp_path.suffix == ".vue":
         ipyvue.register_component_from_string(
             name=comp_path.stem.replace('_', '-'),
             value=comp_path.read_text())
