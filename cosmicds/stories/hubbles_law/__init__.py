0eb996a6391434cfc30e49133c58333986030a51
Jon Carifio <carifio24@gmail.com>
2022-06-28 16:07:20 -0400

    Update Hubble-level __init__ to account for folder reorganization.

--- cosmicds/stories/hubbles_law/__init__.py
+++ cosmicds/stories/hubbles_law/__init__.py
@@ -5,10 +5,10 @@ from .tools import *
 
 import ipyvue
 
-vue_comp_dir = Path(__file__).parent / "vue_components"
+vue_comp_dir = Path(__file__).parent / "components"
 
 for comp_path in vue_comp_dir.iterdir():
-    if comp_path.suffix == ".vue":
+    if comp_path.is_file and comp_path.suffix == ".vue":
         ipyvue.register_component_from_string(
             name=comp_path.stem.replace('_', '-'),
             value=comp_path.read_text())
