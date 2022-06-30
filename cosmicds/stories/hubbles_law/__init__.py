df77c3563d180b43f93e92429e398662a3322481
Jon Carifio <carifio24@gmail.com>
2022-06-30 18:38:21 -0400

    Change vue_comp_dir -> comp_dir in the Hubble's law story __init__.py

--- cosmicds/stories/hubbles_law/__init__.py
+++ cosmicds/stories/hubbles_law/__init__.py
@@ -5,9 +5,9 @@ from .tools import *
 
 import ipyvue
 
-vue_comp_dir = Path(__file__).parent / "components"
+comp_dir = Path(__file__).parent / "components"
 
-for comp_path in vue_comp_dir.iterdir():
+for comp_path in comp_dir.iterdir():
     if comp_path.is_file and comp_path.suffix == ".vue":
         ipyvue.register_component_from_string(
             name=comp_path.stem.replace('_', '-'),
