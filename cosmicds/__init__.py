905db77e902c261da121cd14c9daf6416cb4bf3b
Nicholas Earl <contact@nicholasearl.me>
2022-08-03 14:21:42 -0500

    Reconfigure initialization to discover plugins

--- cosmicds/__init__.py
+++ cosmicds/__init__.py
@@ -1,7 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 from .version import __version__
+import sys
+import logging
+
 __all__ = []
+
+log = logging.getLogger()
 
 # Register any custom Vue components
 from pathlib import Path
@@ -15,5 +20,17 @@ for comp_path in comp_dir.iterdir():
             name=comp_path.stem.replace('_', '-'),
             value=comp_path.read_text())
 
-from .stories import *
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
+
+discovered_plugins = entry_points(group='cosmicds.plugins')
+
+STORY_PATHS = {}
+
+for ep in discovered_plugins:
+    ep.load()
+    log.info("Discovered the `%s` data story.", ep.name)
+
 from .tools import *
