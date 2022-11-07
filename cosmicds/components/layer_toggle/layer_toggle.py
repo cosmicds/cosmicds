66d7e568264c8847bedc00adfbd832a38d483ac9
Carifio24 <carifio24@gmail.com>
2022-11-07 10:12:23 -0500

    Add the option to ignore layers based on conditions.

--- cosmicds/components/layer_toggle/layer_toggle.py
+++ cosmicds/components/layer_toggle/layer_toggle.py
@@ -1,4 +1,5 @@
 from echo import delay_callback
+from echo.callback_container import CallbackContainer
 from ipyvuetify import VuetifyTemplate
 from traitlets import List, observe
 
@@ -15,8 +16,16 @@ class LayerToggle(VuetifyTemplate):
         self.viewer = viewer
         self.name_transform = LayerToggle._create_name_transform(names)
 
+        self._ignore_conditions = CallbackContainer()
+
         self._update_layers_from_viewer()
         self.viewer.state.add_callback('layers', self._update_layers_from_viewer)
+
+    def _ignore_layer(self, layer):
+        for cb in self._ignore_conditions:
+            if cb(layer):
+                return True
+        return False
 
     def _layer_data(self, layer):
         return {
@@ -24,16 +33,29 @@ class LayerToggle(VuetifyTemplate):
             "label": self.name_transform(layer.layer.label)
         }
 
+    @property
+    def watched_layers(self):
+        return [layer for layer in self.viewer.layers if not self._ignore_layer(layer)]
+
+    def add_ignore_condition(self, condition):
+        self._ignore_conditions.append(condition)
+        self._update_layers_from_viewer()
+
+    def remove_ignore_condition(self, condition):
+        self._ignore_conditions.remove(condition)
+        self._update_layers_from_viewer()
+
     def _update_layers_from_viewer(self, layers=None):
-        self.layers = [self._layer_data(layer) for layer in self.viewer.layers]
+        self.layers = [self._layer_data(layer) for layer in self.watched_layers]
         self.selected = [index for index, layer in enumerate(self.viewer.layers) if layer.state.visible]
 
     @observe('selected')
     def _on_selected_change(self, change):
         selected = change["new"]
         with delay_callback(self.viewer.state, 'layers'):
-            for index in range(len(self.layers)):
-                self.viewer.layers[index].state.visible = index in selected
+            for index, layer in enumerate(self.watched_layers):
+                if not self._ignore_layer(layer):
+                    layer.state.visible = index in selected
 
     @staticmethod
     def _create_name_transform(namer):
