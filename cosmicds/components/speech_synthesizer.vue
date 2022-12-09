a61183b111f6aa823c9f54f9f51edf0db6c43bcd
Carifio24 <carifio24@gmail.com>
2022-12-09 17:01:20 -0500

    Change from using element tags to selectors.

--- cosmicds/components/speech_synthesizer.vue
+++ cosmicds/components/speech_synthesizer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-icon
-    @click="sayText"
+    @click="handlePress"
   >
     {{ speaking ? 'mdi-stop' : 'mdi-voice' }}
   </v-icon>
@@ -9,22 +9,38 @@
 <script>
 module.exports = {
   props: {
-    tags: {
+    selectors: {
       type: Array,
       default: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
     },
     stopOnClose: {
       type: Boolean,
       default: true
+    },
+    root: {
+      type: [Object, Function],
+      default: null
     }
   },
   data: function () {
     return {
       speaking: false,
+      intervalID: 0,
+      rootElement: null
     };
+  },
+  mounted() {
+    if (this.root instanceof Element) {
+      this.rootElement = this.root;
+    } else if (this.root instanceof Function) {
+      this.rootElement = this.root();
+    } else {
+      this.rootElement = this.$parent.$el;
+    }
   },
   destroyed() {
     if (this.stopOnClose && this.speaking) {
+      clearInterval(this.intervalID);
       window.speechSynthesis.cancel();
     }
   },
@@ -32,29 +48,66 @@ module.exports = {
     elementText(element) {
       return element.textContent;
     },
+    makeUtterance(text) {
+      const utterance = new SpeechSynthesisUtterance(text);
+
+      // The interval is to work around this issue:
+      // https://bugs.chromium.org/p/chromium/issues/detail?id=679437
+      //
+      // We could just use one interval for the entire block of text items
+      // but the pause-resume is sometimes slightly audible, so better to minimize that.
+      // Note that the pause-resume won't happen if the text takes longer than 14 seconds to say
+      utterance.onstart = (_event) => {
+        this.intervalID = setInterval(() => {
+          window.speechSynthesis.pause();
+          window.speechSynthesis.resume();
+        }, 14000);
+      }
+      utterance.onend = (_event) => {
+        clearInterval(this.intervalID);
+      }
+
+      return utterance;
+    },
+    
+    // Taken from https://www.geeksforgeeks.org/how-to-check-if-an-element-is-visible-in-dom/
+    // TODO: Is there a better way to check?
+    //
+    // Note that element.checkVisibility() doesn't work on Safari:
+    // https://caniuse.com/mdn-api_element_checkvisibility
+    isElementVisible(element) {
+      return element.offsetWidth || 
+             element.offsetHeight || 
+             element.getClientRects().length;
+    },
     getSpeechItems() {
-      const root = this.$parent.$el;
-      const tagElements = this.tags.map(tag => [...root.getElementsByTagName(tag)]);
-      const elements = [].concat(...tagElements);
-      const items = elements.map(element => this.elementText(element));
+      const selectedElements = this.rootElement.querySelectorAll(this.selectors.join(","));
+      const elements = [].concat(...selectedElements).filter(this.isElementVisible);
+      const items = elements.map(element => this.elementText(element)).filter(text => text.length > 0);
       return items;
     },
-    sayText() {
+    handlePress() {
       const synth = window.speechSynthesis;
       if (synth.speaking) {
         synth.cancel();
+        if (this.speaking) {
+          clearInterval(this.intervalID);
+          this.speaking = false;
+          return;
+        }
+      } else {
         this.speaking = false;
-        return;
       }
 
-      // We say each speech item as its own utterance to try to get around this issue:
-      // https://bugs.chromium.org/p/chromium/issues/detail?id=679437
-      //
-      // It also has the nice benefit of giving a better pause between paragraphs
+      // We say each speech item as its own utterance
+      // This gives a nice pause between paragraphs
       this.speaking = true;
       const items = this.getSpeechItems();
-      const utterances = items.map((item) => new SpeechSynthesisUtterance(item));
-      utterances[utterances.length - 1].onend = (_event) => {
+      const utterances = items.map(this.makeUtterance);
+      const lastUtterance = utterances[utterances.length - 1];
+      const lastOnEnd = lastUtterance.onend;
+      utterances[utterances.length - 1].onend = (event) => {
+        lastOnEnd(event);
         this.speaking = false;
       }
       utterances.forEach((utterance) => synth.speak(utterance));
