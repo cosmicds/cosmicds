e34eaafe504ab9a505eea3e56842af593cc289ca
Harry Houghton <henry.houghton@cfa.harvard.edu>
2023-01-04 17:11:22 -0500

    Update scaffold_alert.vue
    - convert the component to a card instead of an alert
    - add opacity overlay to footer
    - move v-if/else statements to column elements, instead of the buttons themselves
    - set column sizes for the v-cols holding text in footer, and no designated column size if v-cols hold buttons.
    - add forward text styling to back text (really only relevant for first scaffold, I think).

--- cosmicds/components/scaffold_alert.vue
+++ cosmicds/components/scaffold_alert.vue
@@ -1,88 +1,106 @@
 <template>             
-  <v-alert
+  <v-card
     color="info"
     class="mb-4 mx-auto"
     max-width="800"
     elevation="6"
   >
-    <v-row>
-      <v-col
-        cols="10"
-      >
-        <h3
-          class="mb-4"
+    <v-card-text>
+      <v-row>
+        <v-col
+          cols="10"
         >
-          {{ header }}
-        </h3>
-      </v-col>
-      <v-col
-        align="right"
-      >
-        <speech-synthesizer/>
-      </v-col>
-    </v-row>
-    <slot></slot>
+          <h3
+            class="mb-4"
+          >
+            {{ header }}
+          </h3>
+        </v-col>
+        <v-col
+          align="right"
+        >
+          <speech-synthesizer/>
+        </v-col>
+      </v-row>
+      <slot></slot>
+    </v-card-text>
 
-    <v-divider
-      class="my-4"
-    >
+    <v-divider>
     </v-divider>
-    
-    <v-row
-      align="center"
-      no-gutters
+
+    <v-card-actions
+      style="background-color: #0004;"
     >
-      <v-col>
-        <v-btn
-          v-if="allowBack"  
-          class="black--text"
-          color="accent"
-          elevation="2"
-          @click="() => { $emit('back'); }"
+      <v-row
+        align="center"
+        class="pa-1"
+        no-gutters
+      >
+        <v-col
+          v-if="allowBack"
+          class="shrink"
         >
-          back
-        </v-btn>
-        <span
+          <div
+            style="font-size: 16px;"
+          >
+            <v-btn
+              class="black--text"
+              color="accent"
+              elevation="2"
+              @click="() => { $emit('back'); }"
+            >
+              back
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col
           v-else
-          style="font-size: 16px;"
+          cols="8"
+          class="shrink"
         >
-          <slot name="back-content"></slot>
-        </span>
-      </v-col>
-      <v-spacer></v-spacer>
-      <v-col
-        v-if="advance"
-        class="shrink"
-      >
-        <div
-          style="font-size: 16px;"
-        >
-          <v-btn
-            class="black--text"
-            color="accent"
-            elevation="2"
-            @click="() => { $emit('next'); }"
+          <div
+            style="font-size: 16px; border-left: solid 3px #FFD740; padding-left: 10px; color: #FFF8E1;"
           >
-            {{ nextText }}
-          </v-btn>
-        </div>
-      </v-col>
-      <v-col
-        v-else
-        cols="6"
-        class="shrink"
-      >
-        <div
-          style="font-size: 16px; border-left: solid 3px #FFD740; padding-left: 10px; color: #FFF8E1;"
+            <slot name="back-content"></slot>
+      </div>
+        </v-col>
+
+        <v-spacer></v-spacer>
+
+        <v-col
+          v-if="advance"
+          class="shrink"
         >
-          <slot
-            name="before-next"
+          <div
+            style="font-size: 16px;"
           >
-          </slot>
-        </div>
-      </v-col>
-    </v-row>
-  </v-alert>
+            <v-btn
+              class="black--text"
+              color="accent"
+              elevation="2"
+              @click="() => { $emit('next'); }"
+            >
+              {{ nextText }}
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col
+          v-else
+          cols="6"
+          class="shrink"
+        >
+          <div
+            style="font-size: 16px; border-left: solid 3px #FFD740; padding-left: 10px; color: #FFF8E1;"
+          >
+            <slot
+              name="before-next"
+            >
+            </slot>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
