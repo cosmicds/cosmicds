8fcc2451736624b39e29b3a3ba60b4e68fd70777
Jon Carifio <carifio24@gmail.com>
2023-05-03 15:39:00 -0400

    Restrict step_index to be at least 0

--- cosmicds/phases.py
+++ cosmicds/phases.py
@@ -85,7 +85,7 @@ class Story(CDSState, HubMixin):
 
     def _on_step_index_changed(self, value):
         self.stages[self.stage_index]['step_index'] = value
-        self.step_index = min(value, len(self.stages[self.stage_index]['steps'])-1)
+        self.step_index = max(min(value, len(self.stages[self.stage_index]['steps'])-1), 0)
         self.step_complete = self.stages[self.stage_index]['steps'][
             self.step_index]['completed']
         self.write_to_db()
