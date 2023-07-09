f409b1c5de27f872a2fbc6e9e291a5ac0a13fbc0
Jon Carifio <carifio24@gmail.com>
2023-07-09 10:15:41 -0400

    Initial implementation of percentage selector.

---
+++ cosmicds/components/statistics_selector/statistics_selector.py
@@ -0,0 +1,37 @@
+from ipyvuetify import VuetifyTemplate
+from traitlets import List, observe
+
+from ...utils import load_template, vertical_line_mark
+
+class StatisticsSelector(VuetifyTemplate):
+
+    template = load_template("statistics_selector.vue", __file__, traitlet=True).tag(sync=True)
+    selected = List().tag(sync=True)
+    statistics = List().tag(sync=True)
+    colors = List(['red', 'orange', 'yellow', 'green', 'blue', 'purple']).tag(sync=True)
+
+    def __init__(self, viewer, data, component_id, layer, statistics=['mean', 'median', 'mode'], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.viewer = viewer
+        self.data = data
+        self.component_id = component_id
+        self.layer = layer
+        self.statistics = statistics
+        self._lines = []
+
+    @observe('selected')
+    def _update_marks(self, change):
+        selected = change["new"]
+        marks = [mark for mark in self.viewer.figure.marks if mark not in self._lines]
+        lines = []
+        for index, stat in enumerate(self.statistics):
+            try:
+                value = self.data.compute_statistic(stat, self.component_id)
+                mark = vertical_line_mark(self.layer, value, self.colors[index])
+                lines.append(mark)
+            except ValueError:
+                continue
+
+        self.viewer.figure.marks = marks + lines
+        self._lines = lines
+
---
+++ cosmicds/components/statistics_selector/statistics_selector.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card
+    flat
+    light
+    variant="outlined"
+    color="white"
+    class="statistics-selector"
+  >
+    <v-list-item-group
+      multiple
+      v-model="selected"
+    >
+      <v-list-item
+        v-for="(stat, index) in statistics"
+        :key="index"
+        :value="stat"
+        inactive
+      >
+        <template v-slot:default="{ active }">
+          <v-list-item-content
+            class="font-weight-bold"
+          >
+            {{ capitalizeFirstLetter(stat) }}
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-checkbox
+              :input-value="active"
+              :color="colors[index]"
+            />
+          </v-list-item-action>
+        </template>
+      </v-list-item>
+    </v-list-item-group>
+  </v-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    capitalizeFirstLetter(text) {
+      return text.charAt(0).toUpperCase() + text.slice(1);
+    }
+  }
+}
+</script>
