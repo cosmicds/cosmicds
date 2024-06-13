4ec59c9f19b0405d9bb3ae409889efd605c11ed3
Carifio24 <carifio24@gmail.com>
2024-06-13 15:15:08 -0400

    Update components init file.

--- src/cosmicds/components/__init__.py
+++ src/cosmicds/components/__init__.py
@@ -4,3 +4,4 @@ from .plotly_support.plotly_support import PlotlySupport
 from .viewer_layout import *
 from .debug_control import StateEditor
 from .layer_toggle import LayerToggle
+from .statistics_selector import StatisticsSelector
