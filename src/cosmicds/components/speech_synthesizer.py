81c3c4ce338ddcadb5e2d682c12fac42f226888d
Carifio24 <carifio24@gmail.com>
2024-08-28 16:12:45 -0400

    Update speech synthesizer Python wrapper.

--- src/cosmicds/components/speech_synthesizer.py
+++ src/cosmicds/components/speech_synthesizer.py
@@ -1,11 +1,10 @@
 import os
 import solara
+from typing import Optional
+
 
 @solara.component_vue(os.path.join("..", "vue_components", "SpeechSynthesizer.vue"))
 def SpeechSynthesizer(
-    autoread: bool,
-    voice: str,
-    pitch: float,
-    rate: float,
+    options: Optional[dict] = None,
 ):
     pass
