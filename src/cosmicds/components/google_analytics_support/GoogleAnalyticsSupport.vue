08cfcad5a1f2144fa0578cb156e7419d8769a36d
Carifio24 <carifio24@gmail.com>
2024-10-15 11:34:52 -0400

    Fix a typo and import analytics component in components init.

--- src/cosmicds/components/google_analytics_support/GoogleAnalyticsSupport.vue
+++ src/cosmicds/components/google_analytics_support/GoogleAnalyticsSupport.vue
@@ -10,7 +10,7 @@ export default {
     activateScript.text = `window.dataLayer = window.dataLayer || [];
                            function gtag(){dataLayer.push(arguments);}
                            gtag('js', new Date());
-                           gtag('config', ${this.tag});`
+                           gtag('config', "${this.tag}");`
     document.head.appendChild(activateScript);
   },
 
