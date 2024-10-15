e9c6f529a30f0dcc63d27405d32c0c1a21540fca
Carifio24 <carifio24@gmail.com>
2024-10-15 11:24:49 -0400

    Add config script as well.

--- src/cosmicds/components/google_analytics_support/GoogleAnalyticsSupport.vue
+++ src/cosmicds/components/google_analytics_support/GoogleAnalyticsSupport.vue
@@ -1,10 +1,17 @@
 <script>
 export default {
   mounted() {
-    const gaScript = document.createElement('script');
-    gaScript.async = true;
-    gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${this.tag}`;
-    document.head.appendChild(gaScript);
+    const fetchScript = document.createElement("script");
+    fetchScript.async = true;
+    fetchScript.src = `https://www.googletagmanager.com/gtag/js?id=${this.tag}`;
+    document.head.appendChild(fetchScript);
+
+    const activateScript = document.createElement("script");
+    activateScript.text = `window.dataLayer = window.dataLayer || [];
+                           function gtag(){dataLayer.push(arguments);}
+                           gtag('js', new Date());
+                           gtag('config', ${this.tag});`
+    document.head.appendChild(activateScript);
   },
 
   props: {
