554fce26a5a0808f760245ea26735519097a9d78
Carifio24 <carifio24@gmail.com>
2024-10-15 15:03:09 -0400

    Try to not use Google Analytics on a local network.

--- src/cosmicds/components/google_analytics_support/GoogleAnalyticsSupport.vue
+++ src/cosmicds/components/google_analytics_support/GoogleAnalyticsSupport.vue
@@ -1,17 +1,27 @@
 <script>
 export default {
   mounted() {
-    const fetchScript = document.createElement("script");
-    fetchScript.async = true;
-    fetchScript.src = `https://www.googletagmanager.com/gtag/js?id=${this.tag}`;
-    document.head.appendChild(fetchScript);
-
-    const activateScript = document.createElement("script");
-    activateScript.text = `window.dataLayer = window.dataLayer || [];
-                           function gtag(){dataLayer.push(arguments);}
-                           gtag('js', new Date());
-                           gtag('config', "${this.tag}");`
-    document.head.appendChild(activateScript);
+    const gaScript = document.createElement("script");
+    gaScript.text = `
+      // Logic derived from https://stackoverflow.com/a/61839170
+      const local = (function (hostname=window.location.hostname) {
+        return (
+          (['localhost', '127.0.0.1', '', '::1'].includes(hostname))
+          || (hostname.startsWith('192.168.'))
+          || (hostname.startsWith('10.'))
+        );
+      })();
+      if (!local) {
+        const script = document.createElement("script");
+        script.async = true;
+        script.src = "https://www.googletagmanager.com/gtag/js?id=${this.tag}";
+        document.head.appendChild(script);
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', "${this.tag}");
+      }`
+    document.head.appendChild(gaScript);
   },
 
   props: {
