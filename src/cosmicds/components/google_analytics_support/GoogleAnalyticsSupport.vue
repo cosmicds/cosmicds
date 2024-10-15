c245c58b315b1af9c8bbb7daef8e1dd0f6a68c58
Carifio24 <carifio24@gmail.com>
2024-10-15 11:17:11 -0400

    Add Google Analytics support component.

---
+++ src/cosmicds/components/google_analytics_support/GoogleAnalyticsSupport.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  mounted() {
+    const gaScript = document.createElement('script');
+    gaScript.async = true;
+    gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${this.tag}`;
+    document.head.appendChild(gaScript);
+  },
+
+  props: {
+    tag: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
