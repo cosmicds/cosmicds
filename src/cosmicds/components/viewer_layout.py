a4520d36382448f0c74bad0eb29bdc290730c62a
Carifio24 <carifio24@gmail.com>
2024-11-21 16:53:53 -0500

    Make ViewerLayout work for non-Plotly viewers.

--- src/cosmicds/components/viewer_layout.py
+++ src/cosmicds/components/viewer_layout.py
@@ -1,4 +1,5 @@
 from cosmicds.utils import make_figure_autoresize, DEFAULT_VIEWER_HEIGHT
+from glue_plotly.viewers.common import PlotlyBaseView
 import solara
 import reacton.ipyvuetify as rv
 
@@ -24,7 +25,8 @@ def ToolBar(viewer):
 
 @solara.component
 def ViewerLayout(viewer, viewer_height=DEFAULT_VIEWER_HEIGHT):
-    make_figure_autoresize(viewer.figure_widget, viewer_height)
+    if isinstance(viewer, PlotlyBaseView):
+        make_figure_autoresize(viewer.figure_widget, viewer_height)
     # viewer.figure_widget.layout.height = 600
     layout = solara.Column(
         children=[
